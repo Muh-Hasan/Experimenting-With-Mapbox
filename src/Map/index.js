2792e9c6daa4ba7414659a1dfa168ff2dbd13f6d
Muh-Hasan <muh.hasan.git@gmail.com>
2020-10-11 12:46:47 +0500

    map done

--- src/Map/index.js
+++ src/Map/index.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useRef, useEffect } from "react";
+import mapboxgl from "mapbox-gl";
+import "./map.css";
 
-export default function Map(){
-    return(
-        <div>
-            <h1>hello</h1>
-        </div>
-    )
+export default function Map() {
+  const mapContainer = useRef(null);
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoibXVoLWhhc2FuIiwiYSI6ImNrZzRzNXh2dDBuZXoyc2xkMmNvbG52MjQifQ.h9QCn3uBMvqX6KCj26yNTQ";
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+    });
+  }, []);
+  return <div className="map-container" ref={mapContainer} />;
 }
