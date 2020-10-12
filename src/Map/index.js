4930ae7676ba00684c5465497e046c48f700261e
Muh-Hasan <muh.hasan.git@gmail.com>
2020-10-12 09:37:15 +0500

    route added

--- src/Map/index.js
+++ src/Map/index.js
@@ -1,6 +1,9 @@
 import React, { useRef, useEffect } from "react";
 import mapboxgl from "mapbox-gl";
 import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
+import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions'
+import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css'
+
 import "./map.css";
 
 export default function Map() {
@@ -15,6 +18,11 @@ export default function Map() {
       zoom: 13,
       center: [4.899, 52.372],
     });
+    var directions = new MapboxDirections({
+      accessToken: token,
+      unit: 'metric',
+      profile: 'mapbox/driving	'
+    });    
     map.addControl(
       new MapboxGeocoder({
         accessToken: mapboxgl.accessToken,
@@ -29,12 +37,7 @@ export default function Map() {
         trackUserLocation: true,
       })
     );
-
-    // map.on("click", (e) => {
-    //   new mapboxgl.Marker()
-    //     .setLngLat([e.lngLat.lng,e.lngLat.lat])
-    //     .addTo(map);
-    // });
+    map.addControl(directions, 'top-left');
     map.addControl(new mapboxgl.NavigationControl(), "bottom-right");
     return () => map.remove();
   }, []);
