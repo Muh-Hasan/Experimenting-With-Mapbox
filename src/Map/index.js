47b9994181c88cecc6aab61c9047cf6811d4c25c
Muh-Hasan <muh.hasan.git@gmail.com>
2020-10-13 10:34:22 +0500

    updated

--- src/Map/index.js
+++ src/Map/index.js
@@ -7,6 +7,8 @@ import "./map.css";
 
 export default function Map() {
   const mapContainer = useRef(null);
+  const buttonFly = useRef(null);
+  const buttonCood = useRef(null);
   const [lon, setLon] = useState(0);
   const [lat, setLat] = useState(0);
   const token =
@@ -38,17 +40,60 @@ export default function Map() {
       unit: "metric",
       profile: "mapbox/driving	",
     });
+    buttonCood.current.addEventListener("click", function () {
+      // map.on("load", function () {
+      map.addSource("route", {
+        type: "geojson",
+        data: {
+          type: "Feature",
+          properties: {},
+          geometry: {
+            type: "LineString",
+            coordinates: [
+              [-77.034084142948, 38.909671288923],
+              [-77.049766, 38.900772],
+              [-77.043929, 38.910525],
+              [-77.0672, 38.90516896],
+              [-77.002583742142, 38.887041080933],
+              [-76.933492720127, 38.99225245786],
+              [-77.097083330154, 38.980979],
+              [-77.359425054188, 38.958058116661],
+              [-77.10853099823, 38.880100922392],
+            ],
+          },
+        },
+      });
+      map.addLayer({
+        id: "route",
+        type: "line",
+        source: "route",
+        layout: {
+          "line-join": "round",
+          "line-cap": "round",
+        },
+        paint: {
+          "line-color": "#888",
+          "line-width": 2,
+        },
+      });
+    });
+    buttonFly.current.addEventListener("click", function () {
+      map.flyTo({
+        center: [-77.034084142948, 38.909671288923],
+        essential: true,
+      });
+    });
     map.addControl(
       new MapboxGeocoder({
         accessToken: mapboxgl.accessToken,
         mapboxgl: mapboxgl,
       })
     );
-    locations.filter((item) => (
+    locations.filter((item) =>
       new mapboxgl.Marker()
         .setLngLat([item.coordinates.lat, item.coordinates.long])
         .addTo(map)
-    ));
+    );
     geolocate.on("geolocate", function (e) {
       var lon = e.coords.longitude;
       var lat = e.coords.latitude;
@@ -60,5 +105,13 @@ export default function Map() {
     map.addControl(new mapboxgl.NavigationControl(), "bottom-right");
     return () => map.remove(); // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  return <div className="map-container" ref={mapContainer} />;
+  return (
+    <>
+      <div className="sidebar pad2">
+        <button ref={buttonFly}>fly</button>
+        <button ref={buttonCood}>Show</button>
+      </div>
+      <div className="map pad2" ref={mapContainer}></div>
+    </>
+  );
 }
